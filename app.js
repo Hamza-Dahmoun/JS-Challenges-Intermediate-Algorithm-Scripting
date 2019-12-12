79b4909563705123a2fd8aacd44329edad90edc3
Hamza <hamza.dahmoun7@gmail.com>
2019-12-12 21:01:33 +0100

    Twelevth Challenge: Sum All Odd Fibonacci Numbers

--- app.js
+++ app.js
@@ -444,3 +444,60 @@ function convertHTML(str) {
 //console.log(convertHTML("Schindler's List"));// should return Schindler&apos;s List.
 //console.log(convertHTML("<>"));// should return &lt;&gt;.
 //console.log(convertHTML("abc"));// should return abc.
+
+//
+//
+/************************************ 12. Sum All Odd Fibonacci Numbers ************/
+/*
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+- The first two numbers in the Fibonacci sequence are 1 and 1.
+- Every additional number in the sequence is the sum of the two previous numbers. 
+- So the first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+*/
+
+function sumFibs(num) {
+    let result =0;
+    if(num==0){
+        result = 0;
+    }
+    else if(num==1){
+        result = 0;
+    }
+    else{
+        result = sumOfOddInArray(getPreviousFibNumbers(num)); 
+    }
+    return result;
+  }
+  function getPreviousFibNumbers(num){
+      let arr= [1,1];
+      let newFib = arr[1] + arr[0];
+      while(newFib <= num){
+        newFib = arr[arr.length-2] + arr[arr.length-1];
+        if(newFib <= num){
+            arr.push(newFib);
+        }          
+      }
+      return arr;
+  }
+  function isOdd(number){
+      if(number%2!=0) return true;
+      else return false;
+  }
+  function sumOfOddInArray(arra){
+      let s = 0;
+      for(let i=0; i<arra.length; i++){
+          if(isOdd(arra[i]))
+          s = s + arra[i];
+      }
+      return s;
+  }
+  
+  //console.log(sumFibs(10));
+  //console.log(sumFibs(1000));// should return 1785.
+  //console.log(sumFibs(4000000));// should return 4613732.
+  //console.log(sumFibs(4));// should return 5.
+  //console.log(sumFibs(75024));// should return 60696.
+  //console.log(sumFibs(75025));// should return 135721.
