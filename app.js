3f2bd657bb1660d105228811ae2b36c84c2c65e2
Hamza <hamza.dahmoun7@gmail.com>
2019-12-12 20:36:38 +0100

    Eleventh Challenge: Convert HTML Entities

--- app.js
+++ app.js
@@ -389,17 +389,58 @@ Check the assertion tests for examples.
 function uniteUnique(...arrays) {
     //console.log("you have passed " + arrays.length + " arrays");
     let uniqueArr = [];
-    for(let i=0; i<arrays.length; i++){
-        for(let j=0; j<arrays[i].length; j++){
-            if(uniqueArr.indexOf(arrays[i][j]) == -1){
+    for (let i = 0; i < arrays.length; i++) {
+        for (let j = 0; j < arrays[i].length; j++) {
+            if (uniqueArr.indexOf(arrays[i][j]) == -1) {
                 //so this is a new number, lets push it to our array
                 uniqueArr.push(arrays[i][j]);
             }
-        }        
+        }
     }
     return uniqueArr;
-  }
-  
-  //console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])) ;//should return [1, 3, 2, 5, 4]
-  //console.log(uniteUnique([1, 2, 3], [5, 2, 1]));// should return [1, 2, 3, 5].
-  //console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));// should return [1, 2, 3, 5, 4, 6, 7, 8]
+}
+
+//console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])) ;//should return [1, 3, 2, 5, 4]
+//console.log(uniteUnique([1, 2, 3], [5, 2, 1]));// should return [1, 2, 3, 5].
+//console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));// should return [1, 2, 3, 5, 4, 6, 7, 8]
+
+//
+//
+/************************************ 11. Convert HTML Entities ************/
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+//
+let htmlResult = ["Result", "&", "<", ">", "\"", "\'"];
+let htmlEntities_andResult = [
+    ["Result", "EntityName"],
+    ["&", "&amp;"],
+    ["<", "&lt;"],
+    [">", "&gt;"],
+    ["\"", "&quot;"],
+    ["\'", "&apos;"]
+];
+function convertHTML(str) {
+    let res = "";
+    for (let i = 0; i < str.length; i++) {
+        let index = htmlResult.indexOf(str[i]);
+        if (index != -1) {
+            //so this character represents an html entity
+
+            //lets find its name and put it in the new string
+            res = res + htmlEntities_andResult[index][1];
+        }
+        else {
+            res = res + str[i];
+        }
+    }
+    return res;
+}
+
+//console.log(convertHTML("Dolce & Gabbana"));// should return Dolce &amp; Gabbana.
+//console.log(convertHTML("Hamburgers < Pizza < Tacos"));// should return Hamburgers &lt; Pizza &lt; Tacos.
+//console.log(convertHTML("Sixty > twelve"));// should return Sixty &gt; twelve.
+//console.log(convertHTML('Stuff in "quotation marks"'));// should return Stuff in &quot;quotation marks&quot;.
+//console.log(convertHTML("Schindler's List"));// should return Schindler&apos;s List.
+//console.log(convertHTML("<>"));// should return &lt;&gt;.
+//console.log(convertHTML("abc"));// should return abc.
